e5f9a4bc466d1c8a8638bdce556b6a5676ecf931
wenyuan zhang <wenyuanzhang@wenyuandebijibendiannao.local>
2022-12-13 09:37:43 -0400

    update

---
+++ chemistry/gaussian_blur.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+def gaussian_blur(input_tensor: tf.Tensor, blur_tensor: tf.Tensor, deviation: float = 0.1) -> tf.Tensor:
+    """
+    blur input tensor by gaussian function
+    output size: (input_tensor, blur_tensor)
+    """
+    if input_tensor.dtype is
+    input_tensor = tf.expand_dims(input_tensor, -1)
+    blur_tensor = tf.expand_dims(blur_tensor, 0)
+
+    gamma = -0.5 / tf.square(deviation)
+
+    return tf.exp(gamma * tf.square(input_tensor - blur_tensor))
+
+
+if __name__ == "__main__":
+    v = tf.Variable([[0, 1, 2], [3, 4, 5], [6, 7, 8]], dtype=tf.float16)
+    v1 = tf.Variable([0, 1, 2], dtype = tf.float16)
--- chemistry/molecule/distance_matrix.py
+++ chemistry/molecule/distance_matrix.py
@@ -1,6 +1,33 @@
-import numpy as np
-def get_distance_matrix(coordinate_list: list, system_name: str = 'cartesian') -> np.ndarry:
+import tensorflow as tf
+
+
+def get_distance_matrix(coordinate_list: tf.Tensor, system_name: str = 'cartesian') -> tf.Tensor:
     """
     return distance matrix from input coordinations
     default return -1
     """
+    tf.debugging.assert_rank_at_least(coordinate_list,2, message="coordinate tensor at least at rank 2")
+
+    tf.debugging.assert_integer(coordinate_list, message="coordinate tensor must be float")
+
+    if system_name == 'cartesian':
+        return _cartensian_distance_matrix(coordinate_list)
+    else:
+        raise NotImplementedError("Unrecognized coordinate system")
+
+
+def _cartensian_distance_matrix(coordinate_list):
+    """
+    calculate
+    """
+    vertex_1 = tf.expand_dims(coordinate_list, -2)
+    vertex_2 = tf.expand_dims(coordinate_list, -3)
+    matrix = tf.sqrt(tf.reduce_sum(tf.square(vertex_1 - vertex_2), axis=-1))
+    return matrix
+
+
+
+
+if __name__ == "__main__":
+    v = tf.Variable([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+    print(get_distance_matrix(v))
